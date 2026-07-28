9d9e67125af8b745b8252f514053ddd10098c968
newyoungbins <dudqlsjyb@naver.com>
2026-07-28 12:54:49 +0900

    빌드 진행

---
+++ BuildProcess/Log.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// C# 포인터->온통 포인터.
+// 포인터 = 주소를 저장하는 변수(타입).
+void Log(const char* message)
+{
+	std::cout << message << "\n";
+}
---
+++ BuildProcess/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+// unity-build.
+// 전방 선언 하는 이유.
+
+// 선언 적어주기.
+// void Log(const char*);
+#include "Log.h"
+
+int main()
+{
+	Log("Hello");
+	//std::cout << "Hello\n";
+	// #include "EndBrace.h"
+}
