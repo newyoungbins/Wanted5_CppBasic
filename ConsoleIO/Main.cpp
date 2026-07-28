1fedb46e94284e5235576b392c291c6e09a6e0a4
newyoungbins <https://github.com/newyoungbins>
2026-07-28 10:03:50 +0900

    프로젝트 설정 및 콘솔 출력

---
+++ ConsoleIO/Main.cpp
@@ -0,0 +1,17 @@
+﻿// 헤더 인클루드.
+// std::cout / std::cin 사용하기 위해 필요함.
+#include <iostream>
+
+// 참고로 using namespace std; -> 수업에서는 사용 안 함
+// std는 범위가 너무 큼
+
+// 진입점(Entry Point)
+int main()
+{
+	// 출력.
+	std::cout << "Hello Game!\n";
+
+	// 바로 종료되지 말라고 추가하는 코드
+	// 입력 대기.
+	std::cin.get();
+}
