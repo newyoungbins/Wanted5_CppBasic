b213a7cfbd034a71cf62540cf5aa8efe12140d46
newyoungbins <dudqlsjyb@naver.com>
2026-07-29 12:33:52 +0900

    배열과 문자열 진행

--- Array/Main.cpp
+++ Array/Main.cpp
@@ -1,11 +1,35 @@
 ﻿#include <iostream>
+//#include <array>
+//#include <vector>
+
+// 배열을 전달 받아서 원소를 출력하는 함수.
+void PrintArray(int* array, int count)
+{
+	for (int ix = 0; ix < count; ++ix)
+	{
+		std::cout << array[ix] << " ";
+	}
+	
+}
 
 int main()
 {
+	//std::array<int, 5> stdArray;	// #include <array> 쓰면 쓸 수 있음.
+	//stdArray.size();
+	
+
 	// 배열.
 	const int length = 5;
 	int array[length] = {};
 	//memset(array, 0, sizeof(int) * length);
+
+	// 동적 배열.
+	int* dArray = new int[length];
+
+	// dArray[0] = 10;   // <- 사용은 다른 배열이랑 똑같이 쓰면 된다.
+
+	// 메모리 해제.
+	delete[] dArray;
 
 	// 배열을 포인터로 다루기.
 	int* ptr = array;
@@ -19,11 +43,12 @@ int main()
 
 	// Range-Based Loop (범위 기반 루프)
 	// C# - foreach 와 비슷하다.
-	for (const int value : array)
-	{
-		std::cout << value << " ";
-	}
-	// std::cout << array[ix] << " ";
+	//for (const int value : array)
+	//{
+	//	std::cout << value << " ";
+	//}
+
+	PrintArray(array, length);
 
 	std::cout << "\n";
 
---
+++ String/Main.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+// 클래스.
+class Player
+{
+public:
+	// 생성자.
+	Player(const char* name)
+	{
+		// 문자열 저장을 위해 동적할당.
+		size_t length = strlen(name) + 1;
+		this->name = new char[length];
+
+		// 문자열 값 복사.
+		strcpy_s(this->name, length, name);
+
+		// 값 저장.
+		//for (size_t ix = 0; ix < length - 1; ++ix)
+		//{
+		//	this->name[ix] = name[ix];
+		//}
+
+		//// 문자열 종료 처리.
+		//this->name[length - 1] = '\0';
+	}
+
+	// 소멸자.
+	~Player()
+	{
+		// name 값이 null인지 확인.
+		// if (name)		// 0은 false 1은 true 
+		// if (nullptr != name)
+		if (nullptr != name)
+		{
+			delete[] name;
+		}
+	}
+
+	// 이름 출력.
+	void PrintName() const
+	{
+		name[3] - 'A';
+		std::cout << name << "\n";
+	}
+
+private:
+	// 이름을 저장할 변수.
+	char* name = nullptr;
+};
+
+int main()
+{
+	Player player("RonnieJ");
+	player.PrintName();
+
+	// 문자열
+	// const wchar_t* wName;  // 언리얼에서 쓰는 단어.
+	const char* name = "Ronnie";
+	//char nameArray[4];
+	//nameArray[0] = 'R';
+	//nameArray[1] = 'o';
+	//nameArray[2] = 'n';
+	//nameArray[3] = '\n';	// 문자열 종료 문자.
+
+	// 문자열 크기(문자 개수).
+	size_t length = strlen(name);
+
+	std::cout << "string length " << length << "\n";
+
+	std::cin.get();
+}
