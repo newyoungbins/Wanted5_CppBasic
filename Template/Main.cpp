7344dbf8bdde6d3af1310a967c29841fd13f8c41
newyoungbins <dudqlsjyb@naver.com>
2026-08-03 11:02:53 +0900

    템플릿 진행

---
+++ Template/Main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+// 두 수를 서로 교환하는 함수.
+template<typename T>
+void Swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+// 클래스 템플릿.
+template<typename T, int size = 10 >
+class Data
+{
+public:
+	Data(const T& newData)
+		: data(newData)
+	{
+
+	}
+
+	T GetData() const { return data; }
+	void SetData(const T& newData) { data = newData; }
+	int GetSize() const { return size; }
+
+private:
+	T data = {};
+};
+
+int main()
+{
+	int a = 10;
+	int b = 30;
+	Swap(a, b);
+
+
+	Data<int, 100> intData(30);
+	std::cout << intData.GetData() << "\n";
+
+	Data<float> pi(3.141592f);
+	std::cout 
+		<< "Size : " << intData.GetSize()
+		<< ", Data : " << intData.GetData();
+
+	std::cin.get();
+}
